d2083ad3cf1787600782003d530752ace9ee7e2e
LUCASbrms <lu180105@gmail.com>
2025-09-16 00:11:22 -0300

    paginacompra

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import RecuperarSenha from "./pages/RecuperarSenha/RecuperarSenha.jsx";
 import ResetarSenha from "./pages/ResetarSenha/ResetarSenha";
 import Login from "./pages/Login"; 
 import Compra from "./pages/Compra";
+import Pagamento from './Pagamento/Pagamento';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Route path="/recuperar-senha" element={<RecuperarSenha />} />
         <Route path="/resetar-senha/:token" element={<ResetarSenha />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/compra" element={<Compra />} /> 
+        <Route path="/compra" element={<Compra />} />
+
       </Routes>
     </Router>
   );
---
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -0,0 +1,51 @@
+// Dentro de /src/components/CheckoutForm/CheckoutForm.jsx
+import React, { useState } from 'react';
+import { PaymentElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import './CheckoutForm.css';
+
+export default function CheckoutForm() {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const [message, setMessage] = useState(null);
+    const [isProcessing, setIsProcessing] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        setIsProcessing(true);
+
+        const { error } = await stripe.confirmPayment({
+            elements,
+            confirmParams: {
+                // URL para onde o cliente será redirecionado após o pagamento
+                return_url: `${window.location.origin}/completion`,
+            },
+        });
+
+        if (error.type === "card_error" || error.type === "validation_error") {
+            setMessage(error.message);
+        } else {
+            setMessage("Ocorreu um erro inesperado.");
+        }
+
+        setIsProcessing(false);
+    };
+
+    return (
+        <form id="payment-form" onSubmit={handleSubmit}>
+            <PaymentElement id="payment-element" />
+            <button disabled={isProcessing || !stripe || !elements} id="submit">
+                <span id="button-text">
+                    {isProcessing ? "Processando..." : "Pagar Agora"}
+                </span>
+            </button>
+            {/* Mostra mensagens de erro */}
+            {message && <div id="payment-message">{message}</div>}
+        </form>
+    );
+}
